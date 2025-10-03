8fdb0220ac769480d8b4e45d410c542fbfb9cda4
Paulo Oliveira <paulocivictor@gmail.com>
2025-10-03 18:00:52 -0300

    comecando a puxar os dados da gespe

---
+++ dados_gespe.js
@@ -0,0 +1,51 @@
+function vagasComprometidas() {
+  
+  // movimentação por oficio
+    /*vaga (matrícula ou ticket)
+    status da movimentação
+    data de "envio"
+    matrícula do efetivo
+    nome
+    unidade origem
+    unidade destino*/
+
+    const of_url_mov = "https://docs.google.com/spreadsheets/d/1R2lktPrsVmoMY_Ivx6jSOIB9GI8ZAWENNdiM4qHIZb0/"
+    const of_aba = "Controle"
+    const of_cod_vaga_1 = "R" //vaga (matrícula ou ticket)
+    const of_cod_vaga_2 = "S" //vaga (matrícula ou ticket)
+    const of_status = "C"//status da movimentação
+    const of_apresentacao = "Y"//data de "envio"
+    const of_mat_ef = "G" //matrícula do efetivo
+    const of_nome = "I"
+    const of_un_origem = "K" //unidade origem
+    const of_un_destino = "M" //unidade destino*/
+  
+  // movimentação a pedido
+    /*vaga (matrícula ou ticket)
+    status da movimentação
+    data da solicitação
+    matrícula do efetivo
+    nome
+    unidade destino*/
+    
+    const ped_url = "https://docs.google.com/spreadsheets/d/1otXsADvf31ja7v9-EBvjSc8bbaJulJVVXtCPOM0gSis/"
+    const ped_aba = "Análise"
+    const ped_cod_vaga = "TEM QUE FAZER UMA REVISÃO NA ABA DE ANÁLISE, TEM ALGUMAS MOVIMENTAÇÕES 'DEFERIDAS' (EX. 31.00350092/2025-15), MAS QUE NÃO TEM O DADO NA COLUNA P. TEM QUE SER DEFINIDO. A MOVIMENTAÇÃO 31.00350745/2025-38 ESTÁ COM STATUS INDEFERIDO, MAS A OBSERVAÇÃO FALA QUE A OPÇÃO 2 FOI DEFERIDA"  //vaga (matrícula ou ticket)
+    const ped_status = "E" //status da movimentação
+    const ped_dt_solit = "Y"//data da solicitação
+    const ped_mat_ef = "C" //matrícula do efetivo
+    const ped_nome = "D" //nome
+    const ped_un_dest = "TEM QUE FAZER UMA REVISÃO NA ABA DE ANÁLISE, TEM ALGUMAS MOVIMENTAÇÕES 'DEFERIDAS' (EX. 31.00350092/2025-15), MAS QUE NÃO TEM O DADO NA COLUNA P. TEM QUE SER DEFINIDO. A MOVIMENTAÇÃO 31.00350745/2025-38 ESTÁ COM STATUS INDEFERIDO, MAS A OBSERVAÇÃO FALA QUE A OPÇÃO 2 FOI DEFERIDA"//unidade destino*/
+  
+  // escolha de vagas
+    /*vaga (matrícula ou ticket)
+    status da movimentação
+    data da solicitação
+    matrícula do efetivo
+    nome
+    ato de escolha
+    unidade destino*/
+    
+    const url_esc_vagas = "https://docs.google.com/spreadsheets/d/1roM44LXtfmzeSjXod1zvgLCSrrmhpitN6EgfnfgoqYM/"
+
+}
